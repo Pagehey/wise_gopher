6c86c062e98c1d3f141e51a7021e94dc325331cc
Pierre Gabriel <pierregabriel@me.com>
2021-06-22 18:32:11 +0200

    initial commit

---
+++ lib/wise_gopher.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "wise_gopher/version"
+
+module WiseGopher
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/wise_gopher/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WiseGopher
+  VERSION = "0.1.0"
+end
