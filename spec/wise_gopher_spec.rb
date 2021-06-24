168f4109af8060dfbf0926530b5ad0f6648e18fd
Pierre Gabriel <pierregabriel@me.com>
2021-06-24 12:01:01 +0200

    Remove example test

--- spec/wise_gopher_spec.rb
+++ spec/wise_gopher_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe WiseGopher do
   it "has a version number" do
     expect(WiseGopher::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
