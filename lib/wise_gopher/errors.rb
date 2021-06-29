32cf5bbb7b7617ebea63a525c64b375a3f9de57d
Pierre Gabriel <pierregabriel@me.com>
2021-06-29 10:39:16 +0200

    Add error classes

--- lib/wise_gopher/errors.rb
+++ lib/wise_gopher/errors.rb
@@ -6,16 +6,64 @@ module WiseGopher
 
   # raised when `execute` is called whereas query needs params
   # or `execute_with` did not provide all params
-  class ParamsRequired < Error
-    # TODO: list required params with their types
+  class ArgumentError < Error
+    attr_reader :params
+
+    def initialize(params)
+      @params = params.map do |name, param|
+        "\"#{name}\" (#{param.type.type})"
+      end.join(", ")
+    end
+
+    def message
+      <<-STR
+      \n
+      The following param are declared in query but were not passed as arguments:
+      #{params}
+      STR
+    end
   end
 
   # raised when result contains more columns than declared
   class UndeclaredColumns < Error
-    # TODO: list unceclared columns found in result
+    attr_reader :column_names
+
+    def initialize(column_names)
+      @column_names = column_names.map do |name|
+        "\"#{name}\""
+      end.join(", ")
+    end
+
+    def message
+      <<-STR
+      \n
+      The following columns where found but were not declared: #{column_names}
+
+      If you need them during query execution but not in result,
+      you should use row's `ignore` method, like this:
+
+      class Query < WiseGopher::Base
+        query "SELECT title, rating FROM articles"
+
+        row do
+          column :title, :string
+          ignore :rating
+        end
+      end
+      STR
+    end
   end
 
   # raised when row is not declared or not given
   class RowClassIsMissing < Error
   end
 end
+
+# connection;
+# class Query < WiseGopher::Base
+#   query "SELECT title, rating FROM articles"
+
+#   row do
+#     column :title, :string
+#   end
+# end; Query.execute
