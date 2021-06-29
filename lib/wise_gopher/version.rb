9d7dafeb0d5aeafba254ab6d4c12a8d6b8f99512
Pierre Gabriel <pierregabriel@me.com>
2021-06-29 12:26:05 +0200

    Fix version file Update Gemfile.lock

--- lib/wise_gopher/version.rb
+++ lib/wise_gopher/version.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 module WiseGopher
-  MAJOR = 0
-  MINOR = 1
-  PATCH = 0
+  module VERSION
+    MAJOR = 0
+    MINOR = 1
+    PATCH = 0
 
-  STRING = [MAJOR, MINOR, PATCH].join(".")
+    STRING = [MAJOR, MINOR, PATCH].join(".")
+  end
 end
